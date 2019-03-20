1b00b7a4760fa79ab1b093dbfb8712009432ad3a
Tatiana Tkachenko <tkachenko.tatiana16@gmail.com>
2019-03-20 09:20:35 +0200

    Add markup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-  <h1>Hello!</h1>
-  <div>
-    Here will be my cute budget tracker!!!
-  </div>
-`;
-
-
+import "../styles/index.scss";
